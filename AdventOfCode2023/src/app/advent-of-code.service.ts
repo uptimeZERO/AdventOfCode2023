aa2a22b71eeae8bf6a51af307e0dd1d33bb98315
uptimeZERO_ <pavilionsahota@gmail.com>
2023-12-01 17:34:17 +0000

    Day 1 part 1

--- AdventOfCode2023/src/app/advent-of-code.service.ts
+++ AdventOfCode2023/src/app/advent-of-code.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AdventOfCodeService {
 
-  constructor() { }
+	constructor() { }
+
+	public static getDay1Part1Answer(inputStrings: string[]) {
+		let sum = 0;
+		const validNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+		for (const inputString of inputStrings) {
+			const inputStringChar = inputString.split('');
+			let numberString = '';
+			for (let i = 0; i < inputStringChar.length; i++) {
+				if (validNumbers.includes(inputStringChar[i])) {
+					numberString = inputStringChar[i];
+					break;
+				}
+			}
+
+			for (let i = (inputStringChar.length - 1); i >= 0; i--) {
+				if (validNumbers.includes(inputStringChar[i])) {
+					numberString = `${numberString}${inputStringChar[i]}`;
+					break;
+				}
+			}
+
+			sum += Number(numberString);
+		}
+
+		return sum;
+	}
 }
--- AdventOfCode2023/src/app/advent-of-code2023/advent-of-code2023.component.ts
+++ AdventOfCode2023/src/app/advent-of-code2023/advent-of-code2023.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AdventOfCodeService } from '../advent-of-code.service';
+import { day1input } from './day1-input';
 
 @Component({
-  selector: 'app-advent-of-code2023',
-  templateUrl: './advent-of-code2023.component.html',
-  styleUrls: ['./advent-of-code2023.component.scss']
+	selector: 'app-advent-of-code2023',
+	templateUrl: './advent-of-code2023.component.html',
+	styleUrls: ['./advent-of-code2023.component.scss']
 })
 export class AdventOfCode2023Component implements OnInit {
-  constructor() {
+	public day1Part1Answer: number;
 
-  }
+	constructor() {
+		this.day1Part1Answer = AdventOfCodeService.getDay1Part1Answer(day1input);
+	}
 
-  ngOnInit(): void {
-    
-  }
+	ngOnInit(): void {
+
+	}
 }
