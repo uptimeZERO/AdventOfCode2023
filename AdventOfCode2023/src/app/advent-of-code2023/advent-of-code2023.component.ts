c18b6c470975fd8386a385796ccfd3afc5162213
uptimeZERO_ <pavilionsahota@gmail.com>
2023-12-01 17:21:10 +0000

    more init

---
+++ AdventOfCode2023/src/app/advent-of-code2023/advent-of-code2023.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advent-of-code2023',
+  templateUrl: './advent-of-code2023.component.html',
+  styleUrls: ['./advent-of-code2023.component.scss']
+})
+export class AdventOfCode2023Component implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
--- AdventOfCode2023/src/app/app.module.ts
+++ AdventOfCode2023/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdventOfCode2023Component } from './advent-of-code2023/advent-of-code2023.component';
+import { AdventOfCodeService } from './advent-of-code.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdventOfCode2023Component,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AdventOfCodeService],
 })
 export class AppModule { }
